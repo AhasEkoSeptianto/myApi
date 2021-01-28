84946bc48adb7cd0391d1a6d835476424d464e1b
AhasEkoSeptianto <ahaseko1@gmail.com>
2021-01-28 18:25:04 +0700

    update

--- server.js
+++ server.js
@@ -9,15 +9,6 @@ const controller = require("./server/Controller/about");
 app.use(cors());
 
 app.route("/api").get(controller.index).post();
-
-if (
-	process.env.NODE_ENV === "production" ||
-	process.env.NODE_ENV === "staging"
-) {
-	app.use(express.static("client/build"));
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname + "/client/build/index.html"));
-	});
-}
+app.route("/api/update").get(controller.update).post();
 
 app.listen(port, () => console.log("run"));
--- server/Controller/about.js
+++ server/Controller/about.js
@@ -15,16 +15,42 @@ module.exports = {
 
 				// use collections
 				const col = db.collection("database");
-
-				// find doc
-				const myDoc = await col.find().toArray((err, result) => {
-					// get data
-					if (err) throw err;
-					// console.log(result);
-					res.send({ application: result });
+				const myDoc = await col.find({}).toArray((err, result) => {
 					console.log(result);
-					client.close();
 				});
+				// get data
+
+				res.send("oke");
+				// res.send({ application: result });
+				client.close();
+			} catch (err) {
+				console.log(err.stack);
+			} finally {
+				await client.close();
+			}
+		}
+		loadDb().catch(console.dir);
+	},
+	update: (req, res) => {
+		const client = new MongoClient(uri, { useNewUrlParser: true });
+		const dbName = "data_portofolio_website_update";
+		// funct to db
+		async function loadDb() {
+			try {
+				await client.connect();
+
+				const db = client.db(dbName);
+
+				// use collections
+				const col = db.collection("database");
+				const myDoc = await col.find({}).toArray((err, result) => {
+					console.log(result);
+				});
+				// get data
+
+				res.send("oke");
+				// res.send({ application: result });
+				client.close();
 			} catch (err) {
 				console.log(err.stack);
 			} finally {
